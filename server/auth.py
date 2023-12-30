21e49b97604ceb167fc9aef3a60a87574a9f22ec
Kodoban <gitaliasub.meshf@simplelogin.fr>
2023-12-30 10:55:24 +0200

    Remove forgotten os module import statement

--- server/auth.py
+++ server/auth.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import select
-import os
 
 from . import db
 from .models import User
